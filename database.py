12cd6dbd423611c5e5a74ca1b31302a310064ae2
Sridharani2007 <sridharani6324@gmail.com>
2026-02-14 20:40:57 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+conn = sqlite3.connect("school.db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Students (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    Age INTEGER,
+    Grade TEXT
+)
+""")
+
+data = [
+    ("Alice", 20, "A"),
+    ("Bob", 21, "B")
+]
+
+cur.execute("DELETE FROM Students")
+
+cur.executemany(
+    "INSERT INTO Students (Name, Age, Grade) VALUES (?, ?, ?)",
+    data
+)
+
+conn.commit()
+
+print("Students:")
+for row in cur.execute("SELECT * FROM Students"):
+    print(row)
+
+cur.execute("DELETE FROM Students WHERE ID = 1")
+conn.commit()
+
+print("\nAfter Deletion:")
+for row in cur.execute("SELECT * FROM Students"):
+    print(row)
+
+conn.close()
